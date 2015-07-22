aadb2135d633a4d2e5a8fe46e653a88eebe45112
Ewan Leaver <ewanleaver@gmail.com>
2015-07-22 13:44:44 +0900

    polished contact form (but non-functional)

--- scripts/support.js
+++ scripts/support.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
     $("#contact-button").click(function(){
-        $("#contact-button").fadeOut(200);
+        $("#contact-button").fadeOut(200, function() {
+            $("#submit-button").fadeIn(500);
+            $("#contact-form-contents").fadeIn(500);
+        });
         
-        $("#submit-button").fadeIn(500);
-        $("#contact-form-contents").fadeIn(500);
-
         $("#support-options-div").animate({
             height: '+=300px'
         }, 500);
